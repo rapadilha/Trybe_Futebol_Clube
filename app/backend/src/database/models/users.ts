6983d7333dcde2aca50c568ac52298d69e34cc5b
Raphael <raphaellfpg@gmail.com>
2022-08-19 16:18:03 -0300

    Adiciona model e migration a Users

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,40 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  public id: number;
+  public email:string;
+  public password:string;
+}
+
+Users.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  underscored: true,
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default Users;
